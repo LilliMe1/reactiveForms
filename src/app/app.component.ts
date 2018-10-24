f28fa2f447b5af78002d9e1710d5f15a0ce2fcf7
phenom_lalitha_vaddi <lalitha.vaddi@phenompeople.com>
2018-10-24 11:01:41 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl, FormArray} from "@angular/forms";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rctforms';
+  productForm;
+  defaultData = {
+    id: '900909099',
+    name: 'MacBook Pro',
+    price: '10K',
+    features: ['Display', 'dsdsd', 'dsds', 'dsddsdewe', 'dsdsddsddwewew'],
+   
+  }
+  constructor(){
+
+    this.productForm = new FormGroup({
+      id: new FormControl(),
+      name: new FormControl(),
+      price: new FormControl(),
+      features: new FormArray([
+    
+      ]),
+      manufactured: new FormGroup({
+        by: new FormControl(),
+        year: new FormControl()
+      })
+      
+    });
+
+    for(let i = 0; i<this.defaultData.features.length; i++){
+      this.productForm.get('features').push(new FormControl)
+    }
+
+  }
+
+  addFeature(){
+    this.productForm.get('features').push(
+      new FormControl('RAM 8GB')
+    )
+  }
+
+  removeFeature(index:number){
+    this.productForm.get('features').removeAt(index)
+  }
+
+  collectData(){
+    console.log(this.productForm.value)
+  }
+
+  reset(){
+    this.productForm.reset();
+  }
+  resetPrice(){
+    this.productForm.get('price').reset()
+  }
+  setValue(){
+    this.productForm.patchValue(this.defaultData)
+  }
 }
